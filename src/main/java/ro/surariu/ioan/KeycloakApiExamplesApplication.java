e057ee2c7d41a5697a3d335064691d51928602c6
surariu <surariu@hpe.com>
2020-09-22 15:40:27 +0300

    Maven module

---
+++ src/main/java/ro/surariu/ioan/KeycloakApiExamplesApplication.java
@@ -0,0 +1,14 @@
+package ro.surariu.ioan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakApiExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakApiExamplesApplication.class, args);
+		System.out.println("Running....");
+	}
+
+}
---
+++ src/test/java/ro/surariu/ioan/KeycloakApiExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.surariu.ioan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakApiExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
